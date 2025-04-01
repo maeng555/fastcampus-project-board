6a5771c6973d75ac4f9fe3902b147931d7e1a8a2
maeng555 <mjy5188@naver.com>
2025-04-01 17:45:01 +0900

    #9 - 도메인 코드를 엔티티로 변환
    1 도메인 클래스가 == 1테이블 매칭되도록 설계했다.
    생성자와 'equals() ' , 'hascode()' 등 도메인 맥락에 맞게끔
    디테일하게 설정함

--- fastcampus-project-board/src/main/java/org/fastcampus/projectboard/domain/ArticleComment.java
+++ fastcampus-project-board/src/main/java/org/fastcampus/projectboard/domain/ArticleComment.java
@@ -1,15 +1,79 @@
 package org.fastcampus.projectboard.domain;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@ToString
+
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class) //testcode할때 필요
+@Entity
 public class ArticleComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter
+    @ManyToOne(optional = false)
     private Article article;
+    //optoinal필수값 cascade는 영향에따라 표현 즉 댓글은 게시글에 영향없이 지우거나 생성하기때문
+    @Setter
+    @Column(nullable = false, length = 500)
     private String content;
 
     //meta data
+    @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(nullable = false, length = 100)
     private String createdBy;
+
+    @LastModifiedDate
     private LocalDateTime modifiedAt;
+
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy;
+
+    protected ArticleComment() {
+    } //@NoArgsConstructor 기능
+
+    private ArticleComment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+    public static ArticleComment of(Article article, String content) {
+        return new ArticleComment(article, content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ArticleComment that = (ArticleComment) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
