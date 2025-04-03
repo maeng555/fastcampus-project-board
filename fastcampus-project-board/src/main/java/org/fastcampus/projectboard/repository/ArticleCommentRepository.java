72379f86e646936ea505b2228b41c4e97359a6e9
maeng555 <mjy5188@naver.com>
2025-04-03 16:11:05 +0900

    #1 - querydls 지원을 이용하여 api 검색 옵션 추가

--- fastcampus-project-board/src/main/java/org/fastcampus/projectboard/repository/ArticleCommentRepository.java
+++ fastcampus-project-board/src/main/java/org/fastcampus/projectboard/repository/ArticleCommentRepository.java
@@ -1,9 +1,30 @@
 package org.fastcampus.projectboard.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.fastcampus.projectboard.domain.ArticleComment;
+import org.fastcampus.projectboard.domain.QArticle;
+import org.fastcampus.projectboard.domain.QArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,//기본 검색을 하기위해
+        QuerydslBinderCustomizer<QArticleComment> //기본 및 부분검색도 하기위해
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root){
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content,root.createdAt,root.createdBy);
+        //필터 검색적용
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
+
 }
